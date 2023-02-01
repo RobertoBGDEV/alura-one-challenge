66929bb92f371d2e71290f27ade163bb98453375
rb_ga <rb_galeana@outlook.com>
2023-02-01 16:39:34 -0600

    Se agrega funcionalidad del boton desencriptar

--- funcionalidad.js
+++ funcionalidad.js
@@ -33,5 +33,48 @@ function EncriptarTexto() {
     document.getElementById("divtexto").innerHTML = textoCodificado;
 }
 
+function DesencriptarTexto() {
+    var array = document.getElementById("texto-ingresado").value;
+    var textoCodificado = "";
+
+    if (array == "") {
+        textoCodificado = '<img src="imagenes/Muñeco.png" alt=""><h3>Ningún mensaje fue encontrado</h3><p>Ingresa el texto que desees encriptar o desencriptar.</p>';
+    } else {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            switch (element) {
+                case "a":
+                    textoCodificado += "a";
+                    index += 1;
+                    break;
+                case "e":
+                    textoCodificado += "e";
+                    index += 4;
+                    break;
+                case "i":
+                    textoCodificado += "i";
+                    index += 3;
+                    break;
+                case "o":
+                    textoCodificado += "o";
+                    index += 3;
+                    break;
+                case "u":
+                    textoCodificado += "u";
+                    index += 3;
+                    break;
+                default:
+                    textoCodificado += element;
+                    break;
+            }
+        }
+    }
+
+    document.getElementById("divtexto").innerHTML = textoCodificado;
+}
+
 var button = document.getElementById("encriptador");
 button.onclick = EncriptarTexto;
+
+var button2 = document.getElementById("desencriptador");
+button2.onclick = DesencriptarTexto;
